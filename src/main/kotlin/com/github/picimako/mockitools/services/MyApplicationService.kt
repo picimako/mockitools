11a32d5bbbc86340346a95b026ecc28a1de0e677
GitHub Action <action@github.com>
2021-10-11 08:21:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/picimako/mockitools/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/picimako/mockitools/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.picimako.mockitools.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.picimako.mockitools.MyBundle
 
 class MyApplicationService {
 
