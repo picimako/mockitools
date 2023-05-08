be9942add84ea274df9913a4c8565edad10c2ccb
Tamas Balog <picimako@protonmail.com>
2023-05-08 14:10:44 +0200

    Update year in licence headers to 2023

--- src/main/java/com/picimako/mockitools/intention/ConvertCallToFieldIntentionBase.java
+++ src/main/java/com/picimako/mockitools/intention/ConvertCallToFieldIntentionBase.java
@@ -1,4 +1,4 @@
-//Copyright 2021 Tamás Balog. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+//Copyright 2023 Tamás Balog. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.picimako.mockitools.intention;
 
