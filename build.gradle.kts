bc7dd76683e85da582e02f889f755b33089f5e22
Tamas Balog <picimako@protonmail.com>
2024-09-07 06:13:03 +0200

    Update dependencies and plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,10 +42,10 @@ changelog {
 }
 
 dependencies {
-    testImplementation("org.assertj:assertj-core:3.26.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.0")
 }
 
 tasks {
