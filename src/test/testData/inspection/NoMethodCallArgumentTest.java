c2d051060fd61454fe3e5e3cd10cbe4b08e0ac90
Tamas Balog <picimako@protonmail.com>
2024-09-09 07:39:31 +0200

    Update an inspection message to be clearer

--- src/test/testData/inspection/NoMethodCallArgumentTest.java
+++ src/test/testData/inspection/NoMethodCallArgumentTest.java
@@ -6,12 +6,12 @@ public class NoMethodCallArgumentTest {
 
     public void testMethod() {
         //Highlight cases
-        Mockito.mock(Object.class, Mockito.withSettings().<error descr="extraInterfaces() is not provided any argument. At least one object must be passed.">extraInterfaces</error>());
-        Mockito.<error descr="verifyNoInteractions() is not provided any argument. At least one object must be passed.">verifyNoInteractions</error>();
-        Mockito.<error descr="verifyNoMoreInteractions() is not provided any argument. At least one object must be passed.">verifyNoMoreInteractions</error>();
-        Mockito.<error descr="verifyZeroInteractions() is not provided any argument. At least one object must be passed.">verifyZeroInteractions</error>();
-        Mockito.<error descr="inOrder() is not provided any argument. At least one object must be passed.">inOrder</error>();
-        Mockito.<warning descr="ignoreStubs() is not provided any argument. At least one object must be passed.">ignoreStubs</warning>();
+        Mockito.mock(Object.class, Mockito.withSettings().<error descr="extraInterfaces() is not provided any argument. At least one type must be passed in.">extraInterfaces</error>());
+        Mockito.<error descr="verifyNoInteractions() is not provided any argument. At least one type must be passed in.">verifyNoInteractions</error>();
+        Mockito.<error descr="verifyNoMoreInteractions() is not provided any argument. At least one type must be passed in.">verifyNoMoreInteractions</error>();
+        Mockito.<error descr="verifyZeroInteractions() is not provided any argument. At least one type must be passed in.">verifyZeroInteractions</error>();
+        Mockito.<error descr="inOrder() is not provided any argument. At least one type must be passed in.">inOrder</error>();
+        Mockito.<warning descr="ignoreStubs() is not provided any argument. At least one type must be passed in.">ignoreStubs</warning>();
 
         //No-highlight cases
         Object mock = Mockito.mock(Object.class, Mockito.withSettings().extraInterfaces(List.class));
